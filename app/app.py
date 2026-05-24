98eb0389e53d5f448d20064d3da4da1ba2c1a94d
sesu808 <saiprasad8112@example.com>
2026-05-24 20:40:32 +0530

    file modified

--- app/app.py
+++ app/app.py
@@ -26,7 +26,7 @@ def hello():
                      alt="heart icon"
                 />
                 <h1 style="color: #e91e63; font-size: 48px;">
-                    Hello Harshi! 🌸, Any one save me?
+                    Hello Harshi! 🌸, Any one save me ⚠️?
                 </h1>
                 <p style="color: #666; font-size: 20px;">
                     Deployed with Kubernetes & Helm
