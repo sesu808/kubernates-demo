6fa32add01518bb7cacc8d41801aeec16c142e55
sesu808 <saiprasad8112@example.com>
2026-05-27 15:30:13 +0530

    file updated

--- app/app.py
+++ app/app.py
@@ -26,7 +26,7 @@ def hello():
                      alt="heart icon"
                 />
                 <h1 style="color: #e91e63; font-size: 48px;">
-                    Harshi! 🌸, thank you for teaching!🩷
+                    lets take up the challenge ✊
                 </h1>
                 <p style="color: #666; font-size: 20px;">
                     Deployed with Kubernetes & Helm
