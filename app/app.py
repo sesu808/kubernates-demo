32d5fa3ed9d3e97a2802cc87ce8c76fe26bf9d98
sesu808 <saiprasad8112@example.com>
2026-05-31 21:00:16 +0530

    file updated

--- app/app.py
+++ app/app.py
@@ -39,7 +39,7 @@ def hello():
 
 @app.route('/health')
 def health():
-    return "ERROR", 500  # readiness fails
+    return "OK", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
